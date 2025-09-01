77056677f500a675904dcbf03de43556e064f721
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-09-01 16:14:58 +0900

    [Init] 프로젝트 초기 설정

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "Home",
+  description: "Welcome to our homepage!",
+};
+
+const HomePage = () => {
+  return (
+    <main>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the main content of the homepage.</p>
+    </main>
+  );
+};
+export default HomePage;
