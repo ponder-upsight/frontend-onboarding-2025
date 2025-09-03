8793d30360818690659263cf33b03130aaa437bc
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-09-03 17:48:50 +0900

    [Feat] 구매 API 구현

--- src/api/products/client/usePostPurchaseProduct.ts
+++ src/api/products/client/usePostPurchaseProduct.ts
@@ -1,6 +1,8 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { QueryKeys } from "../QueryKeys";
 import { publicAxiosInstance } from "@/util/fetchUtil/axionsInstance";
+import useCartStore from "@/lib/zustand/useCartStore";
+import { useRouter } from "next/router";
 
 interface PostPurchaseProductProps {
   productId: string;
@@ -18,12 +20,23 @@ const postPurchaseProduct = async ({
 };
 export const usePostPurchaseProduct = () => {
   const queryClient = useQueryClient();
+  const { removeItem } = useCartStore();
+  const router = useRouter();
   return useMutation({
     mutationFn: (postPurchaseProductProps: PostPurchaseProductProps) =>
       postPurchaseProduct(postPurchaseProductProps),
-    onSuccess: () => {
+
+    onError: (error) => {
+      console.error("상품 구매 중 오류 발생:", error);
+      alert("상품 구매 중 오류가 발생했습니다. 다시 시도해주세요.");
+    },
+    onSuccess: (data, variables) => {
+      const { productId } = variables;
       // 상품 생성 성공 시, 상품 목록 캐시를 무효화하여 최신 데이터로 갱신
       queryClient.invalidateQueries({ queryKey: [QueryKeys.PRODUCTS] });
+      removeItem(productId);
+      alert("상품이 성공적으로 구매되었습니다.");
+      router.push("/");
     },
   });
 };
